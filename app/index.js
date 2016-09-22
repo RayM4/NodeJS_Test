6db3cee56b74ab7b17d9876b1d5f2043ae20ac53
ray <raymak1479@gmail.com>
2016-09-21 23:25:40 -0400

    test server created

---
+++ app/index.js
@@ -0,0 +1,16 @@
+// app/index.js
+const http = require('http');
+const port = 3000;
+
+const requestHandler = (request, response) => {
+  console.log(request.url);
+  response.end("Hello connection established.");
+}
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+  if (err)
+    return console.log("Error: ", err);
+  console.log("Listening on port: ${port}")
+});
