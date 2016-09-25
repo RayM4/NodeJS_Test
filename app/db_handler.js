b63653f21d6b49403b31121fbf148fb2e25b3eca
ray <raymak1479@gmail.com>
2016-09-25 19:44:11 -0400

    callbacks

--- app/db_handler.js
+++ app/db_handler.js
@@ -1,18 +1,35 @@
+module.exports = function() {
+  const pg = require('pg');
+  const conn = require('../settings/db_config');
 
-const pg = require('pg');
-const conn = require('../settings/db_config');
 
+// pg.connect(conn.database.url, function(err, client, done) {
+//   if (err)
+//     return console.error('error fetching client', err);
+//
+//   client.query('SELECT * FROM users', function(err, result) {
+//     done();
+//     if (err)
+//       return console.error('error with query', err)
+//     console.log(result.rows);
+//     // res.json(result.rows);
+//     process.exit(0);
+//   });
+// });
 
-pg.connect(conn.database.url, function(err, client, done) {
-  if (err)
-    return console.error('error fetching client', err);
+  function getData()  {
+    pg.connect(conn.database.url, function (err, client) {
+      if (err)
+        return console.error('error fetching client', err);
 
-  client.query('SELECT * FROM users', function(err, result) {
-    done();
-    if (err)
-      return console.error('error with query', err)
-    console.log(result.rows);
-    process.exit(0);
-  });
-});
+      client.query('SELECT * FROM pokemon', function (err, result) {
+        if (err)
+          return console.error('error with query', err)
 
+        return result.rows;
+      });
+    });
+  }
+
+  return module;
+}
--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const port = 3000;
 //postgres
 const pg = require('pg');
 const conn = require('./settings/db_config');
+// const db_handler = require('./app/db_handler');
 
 // Route Handlers
 app.get('/', (request, response) => {
@@ -19,22 +20,12 @@ app.get('/', (request, response) => {
   })
 });
 
-// app.engine('.hbs', exphbs({
-//   defaultLayout: 'main',
-//   extname: '.hbs',
-//   layoutsDir: path.join(__dirname, 'views/layouts')
-// }));
-
-// app.set('view engine', '.hbs');
-// app.set('views', path.join(__dirname, 'views'));
-
-app.get('/data', function(req, res, next) {
-  pg.connect(conn.database.url, function(err, client, done) {
+app.get('/data', function(req, res) {
+  pg.connect(conn.database.url, function(err, client) {
     if (err)
       return console.error('error fetching client', err);
 
     client.query('SELECT * FROM pokemon', function(err, result) {
-      // next();
       if (err)
         return console.error('error with query', err)
       // console.log(result.rows);
@@ -43,12 +34,34 @@ app.get('/data', function(req, res, next) {
       res.render('formatted_table', {
         data: formatTableToHTML(result.rows)
       });
-      // process.exit(0);
-      // next();
     });
   });
 });
 
+
+//testing callbacks
+var cb01 = function(req, res, next) {
+  console.log("first callback");
+  res.locals.stuff = "from cb01";
+  next();
+}
+
+var cb02 = function(req, res, next) {
+  console.log("second callback");
+  res.locals.stuff2 = "from cb02";
+  next();
+}
+
+app.get('/callbacks', [cb01, cb02], function(req, res, next) {
+  console.log("response");
+  next();
+}, function(req, res) {
+  console.log('rendering');
+  res.send("Hello World! <br>" + res.locals.stuff + "<br>" + res.locals.stuff2);
+});
+
+
+//path
 app.engine('.hbs', exphbs({
   defaultLayout: 'main',
   extname: '.hbs',
@@ -58,6 +71,7 @@ app.engine('.hbs', exphbs({
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+//other functions
 var formatTableToHTML = function(data) {
   var rowHeader = "<div class = 'row'>";
   var colHeader = "<div class = 'col-sm-6'>";
