4d8ddcb0f84029cfa551cb633562a454eaf0a0f0
ray <raymak1479@gmail.com>
2016-10-13 23:38:21 -0400

    promises and callbacks

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const port = 3000;
 
 //postgres
 const pg = require('pg');
+const promise = require('promise');
 const conn = require('./settings/db_config');
 // const db_handler = require('./app/db_handler');
 
@@ -49,7 +50,7 @@ app.get('/stat/:dexID', function(req, res) {
     client.query(sql, function(err, result) {
       if (err)
         return console.error('error with query', err);
-      console.log(result.rows);
+      // console.log(result.rows);
       res.render('table_stat', {
         data: formatTableToHTML2(result.rows)
       });
@@ -67,7 +68,7 @@ app.get('/ability/:dexID', function(req, res) {
     client.query(sql, function(err, result) {
       if (err)
         return console.error('error with query', err);
-      console.log(result.rows);
+      // console.log(result.rows);
       res.render('table_ability', {
         data: formatTableToHTML2(result.rows)
       });
@@ -75,18 +76,22 @@ app.get('/ability/:dexID', function(req, res) {
   });
 });
 
+app.get('/move/:dexID', function(req, res) {
+
+});
+
 //testing callbacks
 var cb01 = function(req, res, next) {
   console.log("first callback");
   res.locals.stuff = "from cb01";
   next();
-}
+};
 
 var cb02 = function(req, res, next) {
   console.log("second callback");
   res.locals.stuff2 = "from cb02";
   next();
-}
+};
 
 app.get('/callbacks', [cb01, cb02], function(req, res, next) {
   console.log("response");
@@ -96,6 +101,29 @@ app.get('/callbacks', [cb01, cb02], function(req, res, next) {
   res.send("Hello World! <br>" + res.locals.stuff + "<br>" + res.locals.stuff2);
 });
 
+//callback + module
+var getAbilityData = function(req, res) {
+    res.locals.abilities = new promise(conn.database.selectAbility(req.params.dexID), function(response, err){
+        if (err)
+            promise.reject(err);
+        else
+            promise.resolve(response);
+    });
+};
+
+app.get('/ability2/:dexID', [getAbilityData], function (req, res) {
+    res.promise(getAbilityData(req, res).then(function(res) {
+            res.render('table_ability', {
+                data: formatTableToHTML2(res.locals.abilities)
+            });
+        }
+    ));
+    // var tableData = res.locals.abilities;
+
+    // res.render('table_ability', {
+    //     data: formatTableToHTML2(tableData)
+    // });
+});
 
 //path
 app.engine('.hbs', exphbs({
